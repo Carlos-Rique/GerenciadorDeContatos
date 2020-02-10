f385c83d48395ca931cef1c0da52620b9c7e7ae5
Carlos Silva <carlos.rique130@gmail.com>
2020-02-10 00:16:18 -0400

    Primeiro commit

---
+++ database/migrations/2020_02_08_115802_create_contatos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contatos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome',50);
+            $table->string('telefone',20);
+            $table->date('datanascimento',10)->nullable();
+            $table->string('email',50);
+            $table->string('foto',70)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contatos');
+    }
+}
